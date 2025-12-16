5565b8f5619596c5254bafc4d9dfc7b86d3cb233
jcarintoc <carlotaarintoc@gmail.com>
2025-12-16 14:22:32 +0800

    feat: add navigation bar

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,21 +1,34 @@
 import { Link, NavLink } from "react-router-dom";
 import { navRoutes } from "./nav-routes";
-import { Input } from "../ui/input";
+import Search from "./Search";
 import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import { Menu } from "lucide-react";
+import NavSheet from "./NavSheet";
 
 const Navbar = () => {
   const activeLinkClass = ({ isActive }: { isActive: boolean }) =>
     cn(
-      "text-neutral-400 hover:text-neutral-600 text-sm",
-      isActive && "text-primary"
+      "text-white text-sm font-secondary hover:opacity-80",
+      isActive && "bg-primary/30 px-4 py-1 rounded-full"
     );
 
   return (
-    <header className="border-b">
-      <div className="max-w-5xl mx-auto flex items-center justify-between gap-4 py-2 px-4">
-        <div className="flex items-center gap-8">
-          <Link to="/">AniView</Link>
-          <nav className="flex items-center gap-4">
+    <header className="absolute top-5 left-1/2 -translate-x-1/2 w-full xl:w-7xl px-5">
+      <div className=" flex items-center justify-between gap-4 py-2 px-2 bg-black/50 backdrop-blur-sm rounded-full shadow">
+        <div className="flex items-center gap-3 md:gap-8">
+          <NavSheet>
+            <Button size={"icon"} className="md:hidden rounded-full">
+              <Menu />
+            </Button>
+          </NavSheet>
+          <Link
+            to="/"
+            className="font-secondary md:px-4 md:py-2 md:bg-primary md:rounded-full text-sm md:text-base"
+          >
+            AniView
+          </Link>
+          <nav className="hidden md:flex items-center gap-6">
             {navRoutes.map((route) => (
               <NavLink
                 key={route.path}
@@ -28,7 +41,7 @@ const Navbar = () => {
           </nav>
         </div>
 
-        <Input className="w-full sm:w-72 border-border" type="text" placeholder="Search" />
+        <Search />
       </div>
     </header>
   );
--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="space-y-4">
       <Navbar />
       <Outlet />
     </div>
