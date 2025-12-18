1806a6de4a77a59fe561daa977965ed91efe2134
jcarintoc <carlotaarintoc@gmail.com>
2025-12-18 10:23:46 +0800

    fix: not found class

--- src/components/anime-not-found.tsx
+++ src/components/anime-not-found.tsx
@@ -2,7 +2,7 @@ import NoAnimeFound from "@/assets/not-found-4.gif";
 
 const AnimeNotFound = () => {
   return (
-    <div className="text-center text-2xl sm:text-3xl font-bold font-secondary flex flex-col gap-8 items-center mt-40 p-4">
+    <div className="text-center text-2xl sm:text-3xl font-medium font-secondary flex flex-col gap-8 items-center mt-40 p-4">
       <div className="overflow-hidden shadow-lg ">
         <img src={NoAnimeFound} alt="No anime found" className="w-full" />
       </div>
